166313e15e58f3f3c8096fc231c219bcd16952f1
Evgenii Bondarenko <gordondalos@gmail.com>
2018-12-08 17:58:19 +0600

    функции и параметры

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-require('./style/main.scss');
+require( './style/main.scss' );
 
 new Vue( {
     el: '#App',
     data: {
+        counter: 0,
         msg: 'Привет вью',
+        url: 'https://www.google.com/',
+        link: '<a href="http://google.com">google2</a>'
     },
-    methods:{
-      changeName: function ( event ) {
-          this.msg = event.target.value;
-      }
+    methods: {
+        changeName: function ( event ) {
+            this.msg = event.target.value;
+        },
+
+        addCount: function (count) {
+            this.counter += count;
+        },
+
+        hover: function (  ) {
+            this.counter = 0;
+        }
+
     }
 } );
